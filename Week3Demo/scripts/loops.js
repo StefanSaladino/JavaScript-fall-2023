11afbdc85ca0e8e098f40e53144cbc0503fd5ef1
Stefan Saladino <200551988@student.georgianc.on.ca>
2023-09-25 21:56:06 -0400

    Loops lab complete

--- Week3Demo/scripts/loops.js
+++ Week3Demo/scripts/loops.js
@@ -7,6 +7,14 @@ var msg = document.querySelector('#courses');
 A simple loop of this structure needs an initializer (counter variable), 
 an exit conditional, and and incrementor/decrementor */
 msg.textContent = courseList;
+for (let i = 0; i < courses.length; i++) {
+    if(i === courses.length - 1){
+        msg.textContent += `and ${courses[i]}.`;
+    }
+    else{
+    msg.textContent += `${courses[i]}, `;
+    }
+}
 
 // STEP 1b: Add a conditional for the last item in the array so we can finish the sentence with a period.
 
@@ -23,10 +31,17 @@ input.value = '';
 input.focus();
 //searchName = searchName.toLowerCase();
 /* STEP 2a: Create a FOR loop to iterate through the cities array */
-
+    for (let i = 0; i < cities.length; i++) {
     /* STEP 2b: Build an IF/ELSE conditional that compares 
     searchName with cities[i] */
-    
+        if(cities[i].toLowerCase === searchName.toLowerCase){
+            result.textContent = `${cities[i]} is in the list.`
+            break;
+        }
+        else{
+            result.textContent = `${searchName} is not in the list.`
+        }
+    }
 });
 
 
@@ -38,12 +53,12 @@ for (var i = 1; i <= num; i++) {
 var sqRoot = Math.sqrt(i);
 /* STEP 3a: Build an IF statement that checks whether the 
 square root of the number is NOT an integer */
-if (true) {
+if (!Number.isInteger(sqRoot)) {
     /* STEP 3b: If the square root of the number is not an integer, 
     we don't want to output it to the paragraph, 
     so skip the rest of the instructions inside this loop and 
     go back up to the FOR, using 'continue' */
-    
+    continue;
 }
 numberList.textContent += i + ' ';
 }
@@ -54,15 +69,45 @@ i = 0;
 var outputWhile = '';
 /* STEP 4a: WHILE loop to iterate through the cities array 
 (created above in STEP 2) */
-
+while (i < cities.length) {
+    if (i === cities.length - 1){
+        outputWhile += `and ${cities[i]}.`;
+    }
+    else {
+        outputWhile += `${cities[i]}, `;
+    }
+    i++;
+}
 console.log(outputWhile);
 
-//i = 0;
+j = 0;
 var outputDo = '';
 /* STEP 4b: DO/WHILE loop - grab the above IF/ELSE and the 
 'i++', then create the very same output with DO/WHILE (uncomment the above 'i = 0', first) */
+do{
+    if (j === cities.length - 1){
+        outputDo += `and ${cities[j]}.`;
+    }
+    else {
+        outputDo += `${cities[j]}, `;
+    }
+    j++;
+} while (j < cities.length)
 
 console.log(outputDo);
 /* …now try to loop through the same array backwards! */
+var k= Number(cities.length - 1);
+var outputReverse = ' ';
+while (k >= 0) {
+    if (k === 0){
+        outputReverse += `and ${cities[k]}.`;
+    }
+    else {
+        outputReverse += `${cities[k]}, `;
+    }
+    k--;
+}
+console.log(outputReverse);
+
 
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Looping_code
